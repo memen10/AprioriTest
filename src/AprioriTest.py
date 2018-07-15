4bea368572ef1e85171ad80ec4d4ce8cedb1735c
森 秀晃 <diyosko7@adsl-172-10-1-183.dsl.sndg02.sbcglobal.net>
2018-07-15 23:52:01 +0900

    added AprioriTest

---
+++ src/AprioriTest.py
@@ -0,0 +1,54 @@
+"""
+参考：
+http://pbpython.com/market-basket-analysis.html
+
+"""
+import pandas as pd
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+
+def encode_units(x):
+    if x <= 0:
+        return 0
+    if x >= 1:
+        return 1
+
+df = pd.read_excel('http://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx')
+print(df.head())
+
+df['Description'] = df['Description'].str.strip()
+df.dropna(axis=0, subset=['InvoiceNo'], inplace=True)
+df['InvoiceNo'] = df['InvoiceNo'].astype('str')
+df = df[~df['InvoiceNo'].str.contains('C')]
+
+
+basket = (df[df['Country'] =="France"]
+          .groupby(['InvoiceNo', 'Description'])['Quantity']
+          .sum().unstack().reset_index().fillna(0)
+          .set_index('InvoiceNo'))
+
+basket_sets = basket.applymap(encode_units)
+basket_sets.drop('POSTAGE', inplace=True, axis=1)
+frequent_itemsets = apriori(basket_sets, min_support=0.07, use_colnames=True)
+
+rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+
+print(rules.head())
+print(rules[ (rules['lift'] >= 6) &
+       (rules['confidence'] >= 0.8) ])
+
+print(basket['ALARM CLOCK BAKELIKE GREEN'].sum())
+print(basket['ALARM CLOCK BAKELIKE RED'].sum())
+
+basket2 = (df[df['Country'] =="Germany"]
+           .groupby(['InvoiceNo', 'Description'])['Quantity']
+           .sum().unstack().reset_index().fillna(0)
+           .set_index('InvoiceNo'))
+
+basket_sets2 = basket2.applymap(encode_units)
+basket_sets2.drop('POSTAGE', inplace=True, axis=1)
+frequent_itemsets2 = apriori(basket_sets2, min_support=0.05, use_colnames=True)
+
+rules2 = association_rules(frequent_itemsets2, metric="lift", min_threshold=1)
+print(rules2[ (rules2['lift'] >= 4) &
+        (rules2['confidence'] >= 0.5)])
